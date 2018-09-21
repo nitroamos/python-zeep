87ee71b0f66062ba4f2dd5233613ef72f3bd1f06
Amos Anderson <amos.anderson@pfizer.com>
2018-09-21 11:17:33 -0400

    customizing zeep to be less validated

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     },
     entry_points={},
     package_dir={'': 'src'},
-    packages=['zeep'],
+    packages=['lessvalidated_zeep'],
     include_package_data=True,
     license='MIT',
     classifiers=[
